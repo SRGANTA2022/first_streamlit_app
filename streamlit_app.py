c090e428ff1552cc3386f797c57a8e1b01da5981
MellyMei Melnyk <106927724+SRGANTA2022@users.noreply.github.com>
2022-06-05 15:00:15 -0500

    Update streamlit_app.py
    Add menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('my parents new healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('kale, spinach and rocket smoothie')
+streamlit.text('Hard Boiled free-range egg')
