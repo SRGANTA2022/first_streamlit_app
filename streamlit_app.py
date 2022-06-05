4b09c573312a3cc363052325d18da62ede26323a
MellyMei Melnyk <106927724+SRGANTA2022@users.noreply.github.com>
2022-06-05 15:04:38 -0500

    Update streamlit_app.py
    Created another header

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.header('Breakfast Menu 🥣 ')
 streamlit.text('Omega 3 & Blueberry Oatmeal🥗 ')
 streamlit.text('🥑 kale, spinach and rocket smoothie 🍞')
 streamlit.text('Hard Boiled free-range egg 🐔 ')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
